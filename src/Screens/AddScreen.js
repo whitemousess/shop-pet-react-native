8ba4bb56049c7af8e6912b47a04e9b67640bcfe4
whitemousess <thangcoiofficial@gmail.com>
2023-08-25 22:07:55 +0700

    add pet

--- src/Screens/AddScreen.js
+++ src/Screens/AddScreen.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import {
   StyleSheet,
-  View,
   Text,
   TouchableWithoutFeedback,
   Keyboard,
   TextInput,
   SafeAreaView,
   ScrollView,
+  RefreshControl,
 } from "react-native";
+import axios from "axios";
 
 // Picker
 import { Picker } from "@react-native-picker/picker";
@@ -16,10 +17,11 @@ import { Picker } from "@react-native-picker/picker";
 import ButtonCustom from "../components/ButtonCustom";
 import UploadImage from "../components/UploadImage";
 
-function AddScreen() {
+function AddScreen({ navigation }) {
   const [type, setType] = useState("");
-  const [data, setData] = useState({ name: "", info: "" });
+  const [data, setData] = useState({ name: "", description: "" });
   const [image, setImage] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handlePressOutside = () => {
     Keyboard.dismiss();
@@ -37,10 +39,49 @@ function AddScreen() {
     setImage(imageUri);
   };
 
+  const submit = async (e) => {
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("image", {
+      uri: image,
+      type: "image/jpeg", // Change to the actual image type
+      name: "image.jpg", // Change to the actual image file name
+    });
+    formData.append("type", type);
+    formData.append("description", data.description);
+
+    await axios
+      .post("http://192.168.1.5:1407/api/pet/add", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          authorization:
+            "Bearer " +
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGQ3NDg4ZDFlYjBiMjEyNzFiZGRjOGIiLCJpYXQiOjE2OTE4Mzg1MTN9.unlTWuKAln8iLccIvWvuJ-Ddjk0qHS5_SjwWlKSXXPQ",
+        },
+      })
+      .then((response) => {
+        navigation.navigate("DogTab");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    setData("");
+    setType("");
+    setImage(null);
+    setRefreshing(false);
+  }, []);
+
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
       showsHorizontalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
     >
       <TouchableWithoutFeedback onPress={handlePressOutside}>
         <SafeAreaView style={styles.container}>
@@ -58,8 +99,8 @@ function AddScreen() {
 
           <TextInput
             style={styles.input}
-            value={data.info}
-            onChangeText={(text) => handleText("info", text)}
+            value={data.description}
+            onChangeText={(text) => handleText("description", text)}
             placeholder="Thông tin ..."
             placeholderTextColor="#ccc"
           />
@@ -72,7 +113,7 @@ function AddScreen() {
             <Picker.Item label="Chó cảnh" value="dogs" />
             <Picker.Item label="Mèo cảnh" value="cats" />
           </Picker>
-          <ButtonCustom label={"Thêm"} onPress={() => {}} />
+          <ButtonCustom label={"Thêm"} onPress={submit} />
         </SafeAreaView>
       </TouchableWithoutFeedback>
     </ScrollView>
